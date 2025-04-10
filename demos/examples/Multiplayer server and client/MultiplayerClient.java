10b264a3e389635c3b0a7c31010d992503c729ea
Graph3r <>
2025-04-10 04:17:19 +0200

    Added a Multiplayer example

---
+++ demos/examples/Multiplayer server and client/MultiplayerClient.java
@@ -0,0 +1,61 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import polyray.multiplayer.Multiplayer;
+import polyray.multiplayer.MultiplayerPacket;
+import polyray.multiplayer.PacketRegistry;
+
+public class MultiplayerClient {
+
+    public Multiplayer m;
+
+    public MultiplayerClient() throws IOException, InterruptedException {
+        this.m = new Multiplayer("localhost", 3050) {
+            @Override
+            public void onReceive(MultiplayerPacket p) {
+                System.out.println(p.getId());
+                if (p instanceof RectanglePacket pd) {
+                    System.out.println(pd.width + " " + pd.height);
+                }
+            }
+        };
+
+        PacketRegistry.register(RectanglePacket.class, RectanglePacket::new);
+        m.start();
+
+        RectanglePacket p = new RectanglePacket();
+        p.width = (int) (Math.random() * 100.0f) + 100;
+        p.height = (int) (Math.random() * 100.0f) + 100;
+        System.out.println("This size: " + p.width + ", " + p.height);
+
+        while (true) {
+            m.send(p);
+            Thread.sleep(1000);
+            m.pollPackets();
+            Thread.sleep(1000);
+        }
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        MultiplayerClient p = new MultiplayerClient();
+    }
+
+    public static class RectanglePacket extends MultiplayerPacket {
+
+        public int width, height;
+
+        @Override
+        public void read(BufferedInputStream in) throws IOException {
+            ByteBuffer buffer = ByteBuffer.wrap(in.readNBytes(8));
+            this.width = buffer.getInt();
+            this.height = buffer.getInt();
+        }
+
+        @Override
+        public void write(BufferedOutputStream out) throws IOException {
+            out.write(ByteBuffer.allocate(8).putInt(width).putInt(height).array());
+        }
+
+    }
+}
