255ccfd9fcdfb0a56c626190b61a8848eeba52fd
Graph3r <>
2025-08-12 08:10:40 +0200

    Modified due to changing ParticleRenderRegistry

--- src/polyray/systems/manager/ParticleManager.java
+++ src/polyray/systems/manager/ParticleManager.java
@@ -2,7 +2,7 @@ package polyray.systems.manager;
 
 import java.util.ArrayList;
 import java.util.Iterator;
-import polyray.modular.RenderObjectBase;
+import polyray.builtin.RenderObject;
 import polyray.systems.ParticleObj;
 import polyray.systems.registry.ParticleRenderRegistry;
 
@@ -10,13 +10,13 @@ public class ParticleManager {
 
     private static final ArrayList<ParticleObj> particles = new ArrayList<>();
 
-    protected static final void updateTicks() {
+    public static final void updateTicks(double dt) {
         Iterator<ParticleObj> iter = particles.iterator();
         while (iter.hasNext()) {
             ParticleObj p = iter.next();
             if (p.time <= 0) {
                 if (p.instance != null) {
-                    ParticleRenderRegistry.getRenderObject(p.getClass()).removeInstance(p.instance);
+                    ParticleRenderRegistry.removeInstance(p.getClass(), p.instance);
                 }
                 iter.remove();
             } else {
@@ -26,26 +26,26 @@ public class ParticleManager {
         }
     }
 
-    protected static final void update(double dt) {
+    public static final void update(double dt) {
         for (ParticleObj p : particles) {
             p.update(dt);
         }
     }
 
-    protected static final void updateInstances() {
+    public static final void updateInstances() {
         for (ParticleObj p : particles) {
             if (p.instance != null) {
                 p.updateInstance();
             }
         }
-        for (RenderObjectBase obj : ParticleRenderRegistry.getRenderObjects()) {
+        for (RenderObject obj : ParticleRenderRegistry.getRenderObjects()) {
             obj.uploadInstances();
         }
     }
-    
+
     public static final void spawn(ParticleObj p) {
         particles.add(p);
         p.instance = ParticleRenderRegistry.createInstance(p.getClass());
-        ParticleRenderRegistry.getRenderObject(p.getClass()).addInstance(p.instance);
+        ParticleRenderRegistry.addInstance(p.getClass(), p.instance);
     }
 }
