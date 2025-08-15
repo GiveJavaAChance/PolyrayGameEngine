da9b9fcd6a8889c9ce3c42d9e520ac09eefc34aa
Graph3r <>
2025-08-15 10:09:35 +0200

    Added more functionality to IDGenerator

--- src/polyray/systems/IDGenerator.java
+++ src/polyray/systems/IDGenerator.java
@@ -1,11 +1,13 @@
 package polyray.systems;
 
 import java.util.BitSet;
+import java.util.Iterator;
 
 public class IDGenerator {
 
     private final BitSet set;
-    
+    private int count = 0;
+
     public IDGenerator() {
         this.set = new BitSet();
     }
@@ -13,13 +15,35 @@ public class IDGenerator {
     public final int getNewID() {
         int id = set.nextClearBit(0);
         set.set(id);
+        count++;
         return id;
     }
 
     public final void freeID(int ID) {
-        if (ID < 0) {
+        if (ID < 0 || !set.get(ID)) {
             return;
         }
         set.clear(ID);
+        count--;
+    }
+    
+    public final void clear() {
+        set.clear();
+        count = 0;
+    }
+
+    public final boolean contains(int ID) {
+        if (ID < 0) {
+            return false;
+        }
+        return set.get(ID);
+    }
+    
+    public final int size() {
+        return count;
+    }
+
+    public final Iterator<Integer> getIDs() {
+        return set.stream().iterator();
     }
 }
