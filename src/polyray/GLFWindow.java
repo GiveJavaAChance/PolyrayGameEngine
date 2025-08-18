6d54bdb59203ca027a4fb6486d98d20554a149dd
Graph3r <>
2025-08-18 14:06:20 +0200

    Added OpenGL 4.6 profile setting

--- src/polyray/GLFWindow.java
+++ src/polyray/GLFWindow.java
@@ -44,7 +44,10 @@ public class GLFWindow {
             throw new IllegalStateException("Unable to initialize GLFW");
         }
 
-        glfwDefaultWindowHints();
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+        
         glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
         if (fullscreen && exclusive) {
             long monitor = glfwGetPrimaryMonitor(); // Get the primary monitor
