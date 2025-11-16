8a03b29c838b6e67bc0f5416b657a39732423aca
Graph3r <>
2025-11-16 17:59:44 +0100

    Rewritten PacketRegistry to be immutable

--- src/polyray/multiplayer/PacketRegistry.java
+++ src/polyray/multiplayer/PacketRegistry.java
@@ -1,34 +1,28 @@
 package polyray.multiplayer;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.function.Supplier;
 
 public class PacketRegistry {
 
-    private static final ArrayList<Supplier<MultiplayerPacket>> constructors = new ArrayList<>();
-    private static final HashMap<Class<? extends MultiplayerPacket>, Integer> classIds = new HashMap<>();
+    private final HashMap<Integer, PacketType> reg;
+    private final HashMap<String, Integer> ids;
 
-    public static synchronized int register(Class<? extends MultiplayerPacket> clazz, Supplier<MultiplayerPacket> constructor) {
-        Integer d = classIds.get(clazz);
-        if(d != null) {
-            return d;
+    public PacketRegistry(PacketType... types) {
+        this.reg = new HashMap<>(types.length);
+        this.ids = new HashMap<>(types.length);
+        int idx = 0;
+        for (PacketType type : types) {
+            reg.put(idx, type);
+            ids.put(type.tag, idx);
+            idx++;
         }
-        int id = constructors.size();
-        constructors.add(constructor);
-        classIds.put(clazz, id);
-        return id;
     }
 
-    public static MultiplayerPacket create(int id) {
-        return constructors.get(id).get();
+    public PacketType getType(int id) {
+        return reg.get(id);
     }
 
-    public static int getId(Class<? extends MultiplayerPacket> clazz) {
-        Integer id = classIds.get(clazz);
-        if (id == null) {
-            throw new IllegalArgumentException("Class not registered: " + clazz.getName());
-        }
-        return id;
+    public int getID(String tag) {
+        return ids.get(tag);
     }
 }
