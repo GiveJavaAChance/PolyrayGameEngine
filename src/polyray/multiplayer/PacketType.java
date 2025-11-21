34f103668dbc9e701511f9e29f46e0e901706c02
Graph3r <>
2025-11-21 02:16:29 +0100

    Added new types of packets (direct packets)

--- src/polyray/multiplayer/PacketType.java
+++ src/polyray/multiplayer/PacketType.java
@@ -15,29 +15,34 @@ public class PacketType<T> {
     public final ObjectSerializer<T> objectSerializer;
     public final ObjectDeserializer<T> objectDeserializer;
     public final Consumer<T> objectHandler;
+    private final boolean isDirect;
 
-    public PacketType(String tag, StatelessSerializer statelessSerializer, StatelessDeserializer statelessDeserializer) {
+    private PacketType(String tag, StatelessSerializer statelessSerializer, StatelessDeserializer statelessDeserializer, ObjectSerializer<T> objectSerializer, ObjectDeserializer<T> objectDeserializer, Consumer<T> objectHandler, boolean isDirect) {
         this.tag = tag;
         this.statelessSerializer = statelessSerializer;
         this.statelessDeserializer = statelessDeserializer;
-        this.objectSerializer = null;
-        this.objectDeserializer = null;
-        this.objectHandler = null;
-    }
-
-    public PacketType(String tag, ObjectSerializer<T> objectSerializer, ObjectDeserializer<T> objectDeserializer, Consumer<T> objectHandler) {
-        this.tag = tag;
-        this.statelessSerializer = null;
-        this.statelessDeserializer = null;
         this.objectSerializer = objectSerializer;
         this.objectDeserializer = objectDeserializer;
         this.objectHandler = objectHandler;
+        this.isDirect = isDirect;
+    }
+
+    public PacketType(String tag, StatelessSerializer statelessSerializer, StatelessDeserializer statelessDeserializer) {
+        this(tag, statelessSerializer, statelessDeserializer, null, null, null, false);
+    }
+
+    public PacketType(String tag, ObjectSerializer<T> objectSerializer, ObjectDeserializer<T> objectDeserializer, Consumer<T> objectHandler) {
+        this(tag, null, null, objectSerializer, objectDeserializer, objectHandler, false);
     }
 
     public Runnable deserialize(int clientID, ByteReader reader) {
         if (objectDeserializer == null) {
             try {
                 int len = reader.getInt();
+                if (isDirect) {
+                    statelessDeserializer.deserialize(clientID, reader);
+                    return null;
+                }
                 byte[] data = reader.getBytes(len);
                 ByteReader r = new ByteReader(new ByteArrayInputStream(data));
                 return () -> {
@@ -51,13 +56,25 @@ public class PacketType<T> {
         }
         try {
             T obj = objectDeserializer.deserialize(clientID, reader);
+            if (isDirect) {
+                objectHandler.accept(obj);
+                return null;
+            }
             return () -> objectHandler.accept(obj);
         } catch (IOException e) {
         }
         return null;
     }
-    
+
     public static PacketType message(String msg, IntConsumer onReceive) {
         return new PacketType(msg, out -> {}, (clientID, in) -> onReceive.accept(clientID));
+    }
+
+    public static PacketType direct(String tag, StatelessSerializer statelessSerializer, StatelessDeserializer statelessDeserializer) {
+        return new PacketType<>(tag, statelessSerializer, statelessDeserializer, null, null, null, true);
+    }
+
+    public static <T> PacketType direct(String tag, ObjectSerializer<T> objectSerializer, ObjectDeserializer<T> objectDeserializer, Consumer<T> objectHandler) {
+        return new PacketType<>(tag, null, null, objectSerializer, objectDeserializer, objectHandler, true);
     }
 }
