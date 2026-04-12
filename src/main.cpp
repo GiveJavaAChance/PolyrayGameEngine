a9b7d94d7d58ee7833a904c43f3d24d0dd0b71b5
Graph3r <alexlindgren01@gmail.com>
2026-04-13 00:49:38 +0200

    New testing setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,49 +1,68 @@
 #include <iostream>
-#include "base/glad/glad.h"
-#include "Allocator.h"
-#include "BindingRegistry.h"
-#include "BindSet.h"
-#include "BitSet.h"
-#include "BVH.h"
-#include "CascadedQueue.h"
-#include "DynamicArray.h"
-#include "ECS.h"
-#include "EventBus.h"
-#include "FFT.h"
-#include "FullscreenQuad.h"
-#include "GLFramebuffer.h"
-#include "GLFramebufferMSAA.h"
-#include "GLFWindow.h"
-#include "GLGbuffer.h"
-#include "GLGbufferMSAA.h"
-#include "GLTexture.h"
-#include "GLTextureMSAA.h"
-#include "HeapBuffer.h"
-#include "IDGenerator.h"
-#include "Input.h"
-#include "InputSystem.h"
-#include "InstancedRenderSystem.h"
-#include "MultiDynamicArray.h"
-#include "perf.h"
-#include "Physics2D.h"
-#include "Physics3D.h"
-#include "prvl.h"
-#include "Registry.h"
-#include "RenderObject.h"
-#include "ResourceManager.h"
-#include "ScriptSystem.h"
-#include "ShaderBuffer.h"
-#include "ShaderBufferHeap.h"
-#include "ShaderManager.h"
-#include "ShaderPreprocessor.h"
-#include "ShaderProgram.h"
-#include "StablePool.h"
-#include "StaticRegistry.h"
-#include "stb_image_ex.h"
-#include "TextRenderer.h"
-#include "Time.h"
-#include "typereg.h"
-#include "UnorderedRegistry.h"
+
+#include <base/glad/glad.h>
+
+#include <input/BindSet.h>
+#include <input/Input.h>
+#include <input/InputSystem.h>
+
+#include <physics/2d/Physics2D.h>
+#include <physics/3d/Physics3D.h>
+
+#include <rendering/GLFramebuffer.h>
+#include <rendering/GLFramebufferMSAA.h>
+#include <rendering/GLGbuffer.h>
+#include <rendering/GLGbufferMSAA.h>
+#include <rendering/GLTexture.h>
+#include <rendering/GLTextureMSAA.h>
+#include <rendering/ShaderBuffer.h>
+#include <rendering/ShaderBufferHeap.h>
+
+#include <scene/2d/Node2D.h>
+#include <scene/2d/SceneNode2D.h>
+#include <scene/2d/Scene2D.h>
+
+#include <scene/3d/Node3D.h>
+#include <scene/3d/SceneNode3D.h>
+#include <scene/3d/Scene3D.h>
+
+#include <shader/ShaderManager.h>
+#include <shader/ShaderPreprocessor.h>
+#include <shader/ShaderProgram.h>
+
+#include <structure/BitSet.h>
+#include <structure/BVH.h>
+#include <structure/CascadedQueue.h>
+#include <structure/DynamicArray.h>
+#include <structure/HeapBuffer.h>
+#include <structure/IDGenerator.h>
+#include <structure/MultiDynamicArray.h>
+#include <structure/Registry.h>
+#include <structure/StablePool.h>
+#include <structure/StaticRegistry.h>
+#include <structure/UnorderedRegistry.h>
+
+#include <utils/FFT.h>
+#include <utils/perf.h>
+
+#include <Allocator.h>
+#include <BindingRegistry.h>
+#include <EventBus.h>
+#include <FullscreenQuad.h>
+#include <GLFWindow.h>
+#include <prvl.h>
+#include <RenderObject.h>
+#include <ResourceManager.h>
+#include <TextRenderer.h>
+#include <typereg.h>
+
+#include <InstancedRenderSystem.h>
+#include <ScriptSystem.h>
+
+#include <ecs/Component.h>
+#include <ecs/ComponentRef.h>
+#include <ecs/ECS.h>
+#include <ecs/Storage.h>
 
 void render(const RenderObject& obj) {
     if(obj.vertexCount == 0u || obj.instanceCount == 0u) {
@@ -81,7 +100,7 @@ EventBus eventBus;
 Camera3D cam;
 Environment env;
 
-#define MODE 1
+#define MODE 0
 
 #if MODE == 0
 struct Vertex3D {
@@ -90,35 +109,86 @@ struct Vertex3D {
     vec2 uv;
 };
 
-struct Instance3D {
-    mat4 instanceTransform;
-};
-
 struct MyScript : Script {
-    double t = 0.0d;
+
+    void physicsUpdate(double dt) {
+        PhysicsObject3D obj;
+        if(getComponent<PhysicsObject3D>(obj)) {
+            obj.accY = -0.1;
+            setComponent(obj);
+        }
+    }
 
     void frameUpdate(double dt) {
-        t += dt;
-        float s = 1.0f * (float) sin(t);
-        if(mat4* t = getComponentPtr<mat4>()) {
+        mat4* t = getComponentPtr<mat4>();
+        PhysicsObject3D obj;
+        if(t && getComponent<PhysicsObject3D>(obj)) {
             mat4& tx = *t;
-            mat3 rot = mat3(vec3(tx[0]), vec3(tx[1]), vec3(tx[2]));
-            rot = rotateX(s) * rotateY(s) * rotateZ(s);
-            tx = mat4(vec4(rot[0], 0.0f), vec4(rot[1], 0.0f), vec4(rot[2], 0.0f), vec4(0.0f, 0.0f, 0.0f, 1.0f));
+            vec4& tr = tx[3];
+            tr.x = static_cast<float>(obj.posX);
+            tr.y = static_cast<float>(obj.posY);
+            tr.z = static_cast<float>(obj.posZ);
         }
     }
 };
 
 InstancedRenderSystem<mat4>* renderSystem;
 ScriptSystem<MyScript>* scriptSystem;
+Physics3D* physicsSystem;
+
+struct AABB {
+};
+
+bool collideAABB_AABB(Collider3D& a, AABB* aData, Collider3D& b, AABB* bData, CollisionInfo3D& out) {
+    double halfAx = a.sizeX * 0.5;
+    double halfAy = a.sizeY * 0.5;
+    double halfAz = a.sizeZ * 0.5;
+    double halfBx = b.sizeX * 0.5;
+    double halfBy = b.sizeY * 0.5;
+    double halfBz = b.sizeZ * 0.5;
+    double ax = a.posX + halfAx;
+    double ay = a.posY + halfAy;
+    double az = a.posZ + halfAz;
+    double bx = b.posX + halfBx;
+    double by = b.posY + halfBy;
+    double bz = b.posZ + halfBz;
+    double dx = ax - bx;
+    double dy = ay - by;
+    double dz = az - bz;
+    double overlapX = halfAx + halfBx - abs(dx);
+    double overlapY = halfAy + halfBy - abs(dy);
+    double overlapZ = halfAz + halfBz - abs(dz);
+    if (overlapX <= 0.0 || overlapY <= 0.0 || overlapZ <= 0.0) {
+        return false;
+    }
+    if(overlapX < overlapY && overlapX < overlapZ) {
+        out.collisionNormalX = (dx < 0.0) ? -1.0 : 1.0;
+        out.collisionNormalY = 0.0;
+        out.collisionNormalZ = 0.0;
+        out.penetrationDepth = overlapX;
+    } else if(overlapY < overlapZ) {
+        out.collisionNormalX = 0.0;
+        out.collisionNormalY = (dy < 0.0) ? -1.0 : 1.0;
+        out.collisionNormalZ = 0.0;
+        out.penetrationDepth = overlapY;
+    } else {
+        out.collisionNormalX = 0.0;
+        out.collisionNormalY = 0.0;
+        out.collisionNormalZ = (dz < 0.0) ? -1.0 : 1.0;
+        out.penetrationDepth = overlapZ;
+    }
+    return true;
+}
 
 int main() {
-    ecs.registerComponentType<mat4, Layout::AoS>();
+    ecs.registerComponentType<mat4>();
     ResourceManager::addLocalResource("res/shaders");
     ResourceManager::addLocalResource("res/textures");
 
     renderSystem = new InstancedRenderSystem<mat4>(&ecs);
     scriptSystem = new ScriptSystem<MyScript>(&ecs);
+    physicsSystem = new Physics3D(&ecs);
+    physicsSystem->registerCollision<AABB, AABB, collideAABB_AABB>();
 
     GLFWindow w("testing");
     w.createFrame(500, 500, false, true, false);
@@ -156,35 +226,77 @@ int main() {
     //uint32_t pixel = 0xFFFFFFFFu;
     //tex.set2DTextureData(&pixel, 1, 1, 0, 0, GL_RGBA, GL_UNSIGNED_BYTE);
     RenderObject obj{shader, &tex};
-    obj.mode = GL_TRIANGLE_STRIP;
+    obj.mode = GL_TRIANGLES;
 
     Vertex3D vertices[]{
-        {{ 10.0f, 0.0f, -10.0f}, {0.0f, 1.0f, 0.0f}, {0.0f, 0.0f}},
-        {{-10.0f, 0.0f, -10.0f}, {0.0f, 1.0f, 0.0f}, {1.0f, 0.0f}},
-        {{ 10.0f, 0.0f,  10.0f}, {0.0f, 1.0f, 0.0f}, {0.0f, 1.0f}},
-        {{-10.0f, 0.0f,  10.0f}, {0.0f, 1.0f, 0.0f}, {1.0f, 1.0f}}
+        {{ 1.0f, 1.0f, -1.0f}, {0.0f, 1.0f, 0.0f}, {0.0f, 0.0f}},
+        {{-1.0f, 1.0f, -1.0f}, {0.0f, 1.0f, 0.0f}, {1.0f, 0.0f}},
+        {{ 1.0f, 1.0f,  1.0f}, {0.0f, 1.0f, 0.0f}, {0.0f, 1.0f}},
+        {{-1.0f, 1.0f, -1.0f}, {0.0f, 1.0f, 0.0f}, {1.0f, 0.0f}},
+        {{-1.0f, 1.0f,  1.0f}, {0.0f, 1.0f, 0.0f}, {1.0f, 1.0f}},
+        {{ 1.0f, 1.0f,  1.0f}, {0.0f, 1.0f, 0.0f}, {0.0f, 1.0f}},
+
+        {{ 1.0f, -1.0f, -1.0f}, {0.0f, -1.0f, 0.0f}, {0.0f, 0.0f}},
+        {{ 1.0f, -1.0f,  1.0f}, {0.0f, -1.0f, 0.0f}, {0.0f, 1.0f}},
+        {{-1.0f, -1.0f, -1.0f}, {0.0f, -1.0f, 0.0f}, {1.0f, 0.0f}},
+        {{-1.0f, -1.0f,  1.0f}, {0.0f, -1.0f, 0.0f}, {1.0f, 1.0f}},
+        {{-1.0f, -1.0f, -1.0f}, {0.0f, -1.0f, 0.0f}, {1.0f, 0.0f}},
+        {{ 1.0f, -1.0f,  1.0f}, {0.0f, -1.0f, 0.0f}, {0.0f, 1.0f}},
+
+        {{ 1.0f, -1.0f, 1.0f}, {0.0f, 0.0f, 1.0f}, {0.0f, 0.0f}},
+        {{-1.0f, -1.0f, 1.0f}, {0.0f, 0.0f, 1.0f}, {1.0f, 0.0f}},
+        {{ 1.0f,  1.0f, 1.0f}, {0.0f, 0.0f, 1.0f}, {0.0f, 1.0f}},
+        {{-1.0f, -1.0f, 1.0f}, {0.0f, 0.0f, 1.0f}, {1.0f, 0.0f}},
+        {{-1.0f,  1.0f, 1.0f}, {0.0f, 0.0f, 1.0f}, {1.0f, 1.0f}},
+        {{ 1.0f,  1.0f, 1.0f}, {0.0f, 0.0f, 1.0f}, {0.0f, 1.0f}},
+
+        {{ 1.0f, -1.0f, -1.0f}, {0.0f, 0.0f, -1.0f}, {0.0f, 0.0f}},
+        {{ 1.0f,  1.0f, -1.0f}, {0.0f, 0.0f, -1.0f}, {0.0f, 1.0f}},
+        {{-1.0f, -1.0f, -1.0f}, {0.0f, 0.0f, -1.0f}, {1.0f, 0.0f}},
+        {{-1.0f,  1.0f, -1.0f}, {0.0f, 0.0f, -1.0f}, {1.0f, 1.0f}},
+        {{-1.0f, -1.0f, -1.0f}, {0.0f, 0.0f, -1.0f}, {1.0f, 0.0f}},
+        {{ 1.0f,  1.0f, -1.0f}, {0.0f, 0.0f, -1.0f}, {0.0f, 1.0f}},
+
+        {{1.0f,  1.0f, -1.0f}, {1.0f, 0.0f, 0.0f}, {0.0f, 0.0f}},
+        {{1.0f, -1.0f, -1.0f}, {1.0f, 0.0f, 0.0f}, {1.0f, 0.0f}},
+        {{1.0f,  1.0f,  1.0f}, {1.0f, 0.0f, 0.0f}, {0.0f, 1.0f}},
+        {{1.0f, -1.0f, -1.0f}, {1.0f, 0.0f, 0.0f}, {1.0f, 0.0f}},
+        {{1.0f, -1.0f,  1.0f}, {1.0f, 0.0f, 0.0f}, {1.0f, 1.0f}},
+        {{1.0f,  1.0f,  1.0f}, {1.0f, 0.0f, 0.0f}, {0.0f, 1.0f}},
+
+        {{-1.0f,  1.0f, -1.0f}, {-1.0f, 0.0f, 0.0f}, {0.0f, 0.0f}},
+        {{-1.0f,  1.0f,  1.0f}, {-1.0f, 0.0f, 0.0f}, {0.0f, 1.0f}},
+        {{-1.0f, -1.0f, -1.0f}, {-1.0f, 0.0f, 0.0f}, {1.0f, 0.0f}},
+        {{-1.0f, -1.0f,  1.0f}, {-1.0f, 0.0f, 0.0f}, {1.0f, 1.0f}},
+        {{-1.0f, -1.0f, -1.0f}, {-1.0f, 0.0f, 0.0f}, {1.0f, 0.0f}},
+        {{-1.0f,  1.0f,  1.0f}, {-1.0f, 0.0f, 0.0f}, {0.0f, 1.0f}}
     };
-    obj.uploadVertices(vertices, 4);
+    obj.uploadVertices(vertices, 36);
 
     uint32_t objectID = renderSystem->addObject(&obj);
 
     Entity e = ecs.createEntity();
     e.addComponent(diag(vec4(1.0)));
     e.addComponent(RenderInstance<mat4>(objectID));
+    e.addComponent(PhysicsObject3D{0.0, 3.0, 0.0, 0.0, 3.0, 0.0, 0.0, 0.0, 0.0});
+    uint32_t id;
+    ecs.getComponentID<PhysicsObject3D>(e, id);
+    e.addComponent(DynamicCollider3D{{id}, physicsSystem->createCollider<AABB>(nullptr, 0.0, 0.0, 0.0, 2.0, 2.0, 2.0, 0.0, 0.0), -1.0, -1.0, -1.0});
     e.addComponent(MyScript{});
 
     Entity e2 = ecs.createEntity();
     e2.addComponent(mat4(
-        vec4( 0.0,  1.0, 0.0,  0.0),
-        vec4(-1.0,  0.0, 0.0,  0.0),
-        vec4( 0.0,  0.0, 1.0,  0.0),
-        vec4(10.0, 10.0, 0.0,  1.0)
+        vec4(10.0, 0.0, 0.0,  0.0),
+        vec4(0.0,  1.0, 0.0,  0.0),
+        vec4(0.0,  0.0, 10.0, 0.0),
+        vec4(0.0, -0.5, 0.0,  1.0)
     ));
     e2.addComponent(RenderInstance<mat4>(objectID));
+    e2.addComponent(physicsSystem->createCollider<AABB>(nullptr, -5.0, -0.5, -5.0, 10.0, 1.0, 10.0, 0.0, 0.0));
 
     glClipControl(GL_LOWER_LEFT, GL_ZERO_TO_ONE);
     glDepthFunc(GL_GREATER);
-    glClearDepth(0.0d);
+    glClearDepth(0.0);
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
@@ -198,7 +310,7 @@ int main() {
     ecs.setup();
 
     float t = 0.0f;
-    double dt = 1.0d / 165.0d;
+    double dt = 1.0 / 165.0;
     while(w.isWindowOpen()) {
         uint64_t startTime = Time::nanoTime();
         if(t == 0.0f || Input::getKey(GLFW_KEY_SPACE)) {
@@ -217,7 +329,7 @@ int main() {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         render(obj);
         w.update();
-        dt = static_cast<double>(Time::nanoTime() - startTime) / 1000000000.0d;
+        dt = static_cast<double>(Time::nanoTime() - startTime) / 1000000000.0;
     }
 }
 #elif MODE == 1
@@ -368,6 +480,161 @@ struct RayEmitter {
     }
 };
 
+int main() {
+    ResourceManager::addLocalResource("res/shaders");
+
+    GLFWindow w("testing");
+    w.createFrame(500, 500, false, true, false);
+
+    glClipControl(GL_LOWER_LEFT, GL_ZERO_TO_ONE);
+    glDepthFunc(GL_GREATER);
+    glClearDepth(0.0);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glEnable(GL_DEPTH_TEST);
+
+    cam.projection = reverseZPerspectiveProjection(1.57079632679f, static_cast<float>(w.getWidth()) / static_cast<float>(w.getHeight()), 0.1f);
+    cam.inverseProjection = inverse(cam.projection);
+
+    cam.cameraTransform = diag(vec4(1.0));
+    cam.inverseCameraTransform = inverse(cam.cameraTransform);
+
+    ShaderBuffer cameraBuffer(GL_DYNAMIC_DRAW);
+    cameraBuffer.setSize(sizeof(Camera3D));
+    cameraBuffer.uploadPartialData(&cam, 1, 0);
+
+    ShaderManager::setValue("CAM3D_IDX", BindingRegistry::bindBufferBase(cameraBuffer, GL_UNIFORM_BUFFER));
+
+    GLuint vertexShader = ShaderManager::compileShaderFile("lidar.vert", GL_VERTEX_SHADER);
+    GLuint fragmentShader = ShaderManager::compileShaderFile("lidar.frag", GL_FRAGMENT_SHADER);
+
+    ShaderProgram shader = ShaderManager::createProgram({vertexShader, fragmentShader});
+
+    RenderObject obj{shader, nullptr};
+    obj.mode = GL_TRIANGLE_STRIP;
+
+    vec2 vertices[]{
+        {0.0f, 0.05f * 1.85f},
+        { 0.05f * 1.85f, -0.05f},
+        {-0.05f * 1.85f, -0.05f}
+    };
+    obj.uploadVertices(vertices, 3);
+
+    ShaderBuffer alphaBuffer(GL_DYNAMIC_DRAW);
+    ShaderManager::setValue("ALPHA_IDX", BindingRegistry::bindBufferBase(alphaBuffer, GL_SHADER_STORAGE_BUFFER));
+    RayEmitter emitter{alphaBuffer};
+
+    constexpr uint32_t count = 1000000u;
+    obj.instanceVbo.setSize(count * sizeof(vec4));
+
+    cam.cameraPos.y = 2.0f;
+
+    ecs.setup();
+
+    GLFramebuffer framebuffer(w.getWidth(), w.getHeight());
+    FullscreenQuad quad;
+    quad.setTexture(&framebuffer.color);
+
+    w.mousePress = [](float x, float y, int button) {
+        mouseDown = true;
+    };
+    w.mouseRelease = [](float x, float y, int button) {
+        mouseDown = false;
+    };
+    w.scroll = [](float x, float y, float amt) {
+        focus *= 1.0f - amt * 0.1f;
+        focus = clamp(focus, 0.1f, 1.0f);
+    };
+
+    dvec2 ang = dvec2(0.0);
+
+    uint32_t idx = 0u;
+
+    double dt = 1.0 / 165.0;
+    while(w.isWindowOpen()) {
+        uint64_t startTime = Time::nanoTime();
+
+        dvec2 size = dvec2(w.getWidth(), w.getHeight());
+        dvec2 center = 0.5 * size;
+        dvec2 p;
+        w.getMousePos(p.x, p.y);
+        w.setMousePos(center.x, center.y);
+        dvec2 d = (p - center) * 2.0f;
+        ang += dvec2(d.y, -d.x) / size;
+        cam.cameraTransform = mat4(rotateX(ang.x) * rotateY(ang.y));
+        cam.inverseCameraTransform = transpose(cam.cameraTransform);
+
+        vec3 forward = -vec3(cam.inverseCameraTransform[2]);
+        forward.y = 0.0f;
+        forward = normalize(forward);
+        vec3 right = vec3(cam.inverseCameraTransform[0]);
+        right.y = 0.0f;
+        right = normalize(right);
+
+        vec3 movement = vec3(0.0);
+        if(Input::getKey(GLFW_KEY_W)) {
+            movement += forward;
+        }
+        if(Input::getKey(GLFW_KEY_S)) {
+            movement -= forward;
+        }
+        if(Input::getKey(GLFW_KEY_D)) {
+            movement += right;
+        }
+        if(Input::getKey(GLFW_KEY_A)) {
+            movement -= right;
+        }
+        float len = length(movement);
+        if(len > 0.0f) {
+            float mul = dt * (Input::getKey(GLFW_KEY_LEFT_SHIFT) ? 4.0f : 2.0f) / len;
+            cam.cameraPos += movement * mul;
+        }
+        if(mouseDown) {
+            for(uint32_t i = 0u; i < 1000u; i++) {
+                float a = 6.28318530718f * r();
+                float rf = sqrt(r()) * focus;
+                float s = sin(a) * rf;
+                float c = cos(a) * rf;
+                vec3 dir = -normalize(s * vec3(cam.inverseCameraTransform[0]) + c * vec3(cam.inverseCameraTransform[1]) + vec3(cam.inverseCameraTransform[2]));
+                vec3 o = cam.cameraPos + (vec3(cam.inverseCameraTransform[0]) - vec3(cam.inverseCameraTransform[1]) - vec3(cam.inverseCameraTransform[2])) * 0.5f;
+                vec4 hit = rayWorld(o, dir);
+                if(hit.w >= 0.0f && hit.w <= 10.0f) {
+                    vec3 pos = o + dir * hit.w;
+                    vec4 p = vec4(pos, encodeNormal(vec3(hit)));
+                    obj.instanceVbo.uploadPartialData(&p, 1, idx++);
+                    obj.instanceCount = max(obj.instanceCount, idx);
+                    emitter.add(o, pos);
+                    if(idx == 1000000u) {
+                        idx = 0u;
+                    }
+                }
+            }
+        }
+
+        emitter.update(dt);
+
+        ecs.update(dt);
+
+        cameraBuffer.uploadPartialData(&cam, 1, 0);
+
+        framebuffer.bind();
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        render(obj);
+        glEnable(GL_DEPTH_TEST);
+        emitter.render();
+
+        framebuffer.unbind();
+
+        glClear(GL_COLOR_BUFFER_BIT);
+        glDisable(GL_DEPTH_TEST);
+        quad.render();
+
+        w.update();
+        dt = static_cast<double>(Time::nanoTime() - startTime) / 1000000000.0;
+    }
+}
+#elif MODE == 2
 int main() {
     ResourceManager::addLocalResource("res/shaders");
 
